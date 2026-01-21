a312589d6860fcce045a774221a1c48c6e9ec319
jhonedwin99 <jhonelejalde@gmail.com>
2026-01-21 23:33:37 +0000

    landingpage2

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./navbar";
 import Container from "./container";
+import Jumbotron from "./jumbotron";
 import Footer from "./footer";
 //create your first component
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Jumbotron />
       <Container />
       <Footer />
     </>
--- src/js/components/container.jsx
+++ src/js/components/container.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import Card from "./card";
-import Jumbotron from "./jumbotron";
 const Container = () => {
     return (
         <>
-            {/* JUMBOTRON */}
-        <Jumbotron />
-            {/* CARDS */}
             <div className="container my-5">
                 <div className="row g-4">
                     <Card  />
